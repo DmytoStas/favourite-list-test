fb24022adbf9da47fa343f0d2dd00f426fa9168a
Dmytro Stas <dima7stas777@gmail.com>
2023-05-21 14:47:22 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import 'basiclightbox/dist/basiclightbox.min.css';
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
 import { renderCardsMarcup } from './helpers/renderCardsMarcup';
 import { refs } from './partials/refs';
 import { openModalHendler } from './helpers/openModalHendler';
@@ -9,3 +9,4 @@ renderCardsMarcup(instruments);
 refs.cardList.addEventListener('click', openModalHendler);
 
 addToFavList();
+// favourite-list-test\node_modules\basiclightbox\dist\basicLightbox.min.css
